e47bfddc77ca18d113abe50e6598115393a405ad
Your Name <you@example.com>
2025-02-18 12:58:03 +0600

    update joke.js and main.js

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -1,10 +1,10 @@
-const getJoke = require("./joke");
-const core = require("@actions/core");
+import getJoke from "./joke";
+import { setOutput } from "@actions/core";
 
 async function run() {
   const joke = await getJoke();
   console.log(joke);
-  core.setOutput("joke-output", joke);
+  setOutput("joke-output", joke);
 }
 
 run();
